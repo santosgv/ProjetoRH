6006a0cbb3291c8de2e32c559a0be2bb2575cd33
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2021-04-21 19:49:46 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,16 +1,24 @@
 import pyodbc
 
-server = 'DESKTOP-O0728HG\SQLEXPRESS'
-database = 'PROJETORH'
-string_conexao = 'Driver={SQL Server Native CLient 11.0};Server=' + server + ';Database=' + database + ';Trusted_Connection=yes;'
-c = pyodbc.connect(string_conexao)
-cur = c.cursor()
+class Bacon():
+    def __init__(self):
+        server = 'DESKTOP-O0728HG\SQLEXPRESS'
+        database = 'PROJETORH'
+        string_conexao = 'Driver={SQL Server Native CLient 11.0};Server=' + server + ';Database=' + database + ';Trusted_Connection=yes;'
+        c = pyodbc.connect(string_conexao)
+        self.cur = c.cursor()
+
+    def consulta(self):
+        show = self.cur.execute(f'''
+                   select * from DEMISSIONAL
+                   ''')
+        for i in show:
+            print(i)
+            return print(i)
+
+    def Cadastra(self):
+        self.cur.execute(f'''
+
+''')
 
 
-def consulta():
-    show = cur.execute(f'''
-           select * from DEMISSIONAL
-           ''')
-    for i in show:
-        print(i)
-    return print(i)
